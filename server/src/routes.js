10c6f467e2157dcfef14f2b51145a59863ad5a7c
mariobatalha <mariobatalha9@gmail.com>
2020-08-21 11:31:16 +0100

    authentication and TDD

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+ const {celebrate, Segments, Joi} = require('celebrate')
 
 const controllerOngs = require('./controllers/controllerOngs');
 const controllerIncidents = require('./controllers/controllerIncidents');
@@ -11,16 +12,47 @@ const routes = express.Router();
 routes.post('/sessions', controllerSession.create);
 
 
-//Ongs routes
+//routes ongs and validation
 routes.get('/ongs', controllerOngs.index);
-routes.post('/ongs', controllerOngs.create);
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(9).max(12),
+        city: Joi.string().required(),
+        province: Joi.string().required(),
+    })
+}), controllerOngs.create);
 
-//Profile
-routes.get('/profile', controllerProfile.index);
+//Routes profile and validation
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        Authorization: Joi.string().required(),
+    }).unknown(),
+}), controllerProfile.index);
 
 //Incidents routes
-routes.post('/incidents', controllerIncidents.create)
-routes.get('/incidents', controllerIncidents.index)
-routes.delete('/incidents/:id', controllerIncidents.delete)
+routes.post('/incidents', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required(),
+        description: Joi.string().required().max(100),
+        value: Joi.number().required(),
+    }),
+    [Segments.HEADERS]: Joi.object({
+        Authorization: Joi.string().required(),
+    }).unknown(),
+}), controllerIncidents.create)
+
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    })
+}), controllerIncidents.index);
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]:Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}), controllerIncidents.delete);
 
 module.exports = routes;
